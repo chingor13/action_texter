f5c275120d6074a76ff214ea1dd4318a8ba16171
Jeff Ching <jching@avvo.com>
2013-12-30 13:53:57 -0800

    change to 0.1.0 for release

--- lib/action_texter/version.rb
+++ lib/action_texter/version.rb
@@ -1,8 +1,8 @@
 module ActionTexter
   module VERSION
-    MAJOR = 3
-    MINOR = 2
-    TINY  = 8
+    MAJOR = 0
+    MINOR = 1
+    TINY  = 0
     PRE   = nil
 
     STRING =  [MAJOR, MINOR, TINY, PRE].compact.join('.')
